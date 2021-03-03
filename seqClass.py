0a17445c2c8c26b1277d53a82e1fce2160808aea
danafd <49127216+danafd@users.noreply.github.com>
2021-03-03 23:03:50 +0200

    Revert "adding exclamation marks"
    This reverts commit 64aa7a698923d017464f69712391c59946fcb879.

--- seqClass.py
+++ seqClass.py
@@ -15,10 +15,10 @@ args = parser.parse_args()
 args.seq = args.seq.upper()
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
-        print ('The sequence is DNA!')
+        print ('The sequence is DNA')
     elif re.search('U', args.seq):
-        print ('The sequence is RNA!')
+        print ('The sequence is RNA')
     else:
-        print ('The sequence can be DNA or RNA!')
+        print ('The sequence can be DNA or RNA')
 else:
-    print ('The sequence is not DNA nor RNA!')
+    print ('The sequence is not DNA nor RNA')
